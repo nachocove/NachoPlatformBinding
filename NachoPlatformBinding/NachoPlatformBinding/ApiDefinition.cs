2c743fb2be645ca2a6cf4cdc8d18813f4cab1265
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-02-25 22:20:34 -0800

    added bindings for Reachability

--- NachoPlatformBinding/NachoPlatformBinding/ApiDefinition.cs
+++ NachoPlatformBinding/NachoPlatformBinding/ApiDefinition.cs
@@ -8,51 +8,31 @@ using MonoTouch.UIKit;
 
 namespace NachoPlatformBinding
 {
+    delegate void ReachabilityBlock (Reachability reachability);
 
-    // Once you've done that, you're ready to move on to binding the API...
-    //
-    //
-    // Here is where you'd define your API definition for the native Objective-C library.
-    //
-    // For example, to bind the following Objective-C class:
-    //
-    //     @interface Widget : NSObject {
-    //     }
-    //
-    // The C# binding would look like this:
-    //
-    //     [BaseType (typeof (NSObject))]
-    //     interface Widget {
-    //     }
-    //
-    // To bind Objective-C properties, such as:
-    //
-    //     @property (nonatomic, readwrite, assign) CGPoint center;
-    //
-    // You would add a property definition in the C# interface like so:
-    //
-    //     [Export ("center")]
-    //     PointF Center { get; set; }
-    //
-    // To bind an Objective-C method, such as:
-    //
-    //     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-    //
-    // You would add a method definition to the C# interface like so:
-    //
-    //     [Export ("doSomething:atIndex:")]
-    //     void DoSomething (NSObject object, int index);
-    //
-    // Objective-C "constructors" such as:
-    //
-    //     -(id)initWithElmo:(ElmoMuppet *)elmo;
-    //
-    // Can be bound as:
-    //
-    //     [Export ("initWithElmo:")]
-    //     IntPtr Constructor (ElmoMuppet elmo);
-    //
-    // For more information, see http://docs.xamarin.com/ios/advanced_topics/binding_objective-c_libraries
-    //
+    [BaseType (typeof(NSObject))]
+    interface Reachability
+    {
+        [Static, Export ("reachabilityForInternetConnection")]
+        Reachability ReachabilityForInternetConnection ();
+
+        [Export ("reachableBlock")]
+        ReachabilityBlock ReachableBlock { get; set; }
+
+        [Export ("unreachableBlock")]
+        ReachabilityBlock UnreachableBlock { get; set; }
+
+        [Export ("isReachable")]
+        bool IsReachable ();
+
+        [Export ("isReachableViaWiFi")]
+        bool IsReachableViaWiFi ();
+
+		[Export ("startNotifier")]
+		void StartNotifier ();
+
+		[Export ("stopNotifier")]
+		void StopNotifier ();
+    }
 }
 
