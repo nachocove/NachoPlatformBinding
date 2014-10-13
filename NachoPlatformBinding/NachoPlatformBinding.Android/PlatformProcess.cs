75d7e07a5a4a6eefe264811c6fc760d6f686f4cd
Henry Kwok <henryk@nachocove.com>
2014-10-13 14:21:58 -0700

    Add a dummy NachoPlatfromBinding.PlatformProcess for Android.

---
+++ NachoPlatformBinding/NachoPlatformBinding.Android/PlatformProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NachoPlatformBinding
+{
+	public class PlatformProcess
+	{
+		public PlatformProcess ()
+		{
+		}
+
+		public static long GetUsedMemory ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
